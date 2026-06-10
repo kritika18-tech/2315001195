19a47990a2a02babaa7a07d83793130cdfd3fe82
kritika18-tech <mishrakritika2818@gmail.com>
2026-06-10 13:01:52 +0530

    Create constant.js

---
+++ logging_middleware_1/constant.js
@@ -0,0 +1,37 @@
+const STACKS = ["backend", "frontend"];
+
+const LEVELS = [
+  "debug",
+  "info",
+  "warn",
+  "error",
+  "fatal"
+];
+
+const PACKAGES = [
+  "cache",
+  "controller",
+  "cron_job",
+  "db",
+  "domain",
+  "handler",
+  "repository",
+  "route",
+  "service",
+  "api",
+  "component",
+  "hook",
+  "page",
+  "state",
+  "style",
+  "auth",
+  "config",
+  "middleware",
+  "utils"
+];
+
+module.exports = {
+  STACKS,
+  LEVELS,
+  PACKAGES
+};
